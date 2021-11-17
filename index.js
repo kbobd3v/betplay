9e82a174fc4d0988cf83b377e7eebd5ead5de0b8
kbdev <leonard@airp.ws>
2021-11-17 13:01:02 -0500

    testing parameters and queries

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 // Importamos express para luego usar su constructor de app
 const express = require('express');
+// Importamos faker para usar informacion de prueba en nuestro project
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
-// Cuando se solicite la ruta / o el index con el verbo GET, asimismo el metodo get recibe
+// Cuando se solicite la ruta / con el verbo GET, este recibe
 // la solicitud y la respuesta como parametros y con send envia la respuesta
 // Es parecido al decorador @app.route en Flask
 app.get('/', (req, res) => {
   res.send('El server funciona con express.js');
 });
 
-app.get('/partidos-hoy', (req, res) => {
-  res.send('Apuesta ya antes que se cierren!');
+// Usamos las queries para obtener la cantidad de informacion que queremos visualizar
+app.get('/matches', (req, res) => {
+  // Creamos un array vacio
+  const matches = [];
+  // Usamos destructuracion ECMAScript tomar los parametros de la query
+  const { size } = req.query;
+  // Si el parametro size no tiene ningun valor entonces limit tendra 10 por defecto
+  const limit = size || 10;
+  // Iteramos para agregar la info de prueba de los partidos al arreglo matches
+  for (let index = 0; index < limit; index++){
+    // el metodo push agrega lo que deseemos al arreglo que llama el metodo
+    matches.push({
+      // name, price e image son metodos que traen informacion de prueba de faker
+      name: faker.commerce.productName(),
+      // el metodo parseInt puede usarse con 2 parametros
+      // El primero es la info a parsear y el 2do es la base del integer
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  // retornamos el json despues de agregar toda la info
+  res.json(matches);
 });
 
 // Tambien podemos enviar archivos json
@@ -29,7 +51,20 @@ app.get('/data', (req, res) => {
     }]);
 });
 
-app.get('/partidos/:id', (req, res) => {
+// Mandamiento
+//Los endpoints especificos deben declararse antes de los endpoints dinamicos.
+
+app.get('/matches/filter', (req, res) => {
+  res.send('La url filter funciona');
+});
+
+// Recuerda que tenemos creado un route que toma el parametro id  luego de la ruta matches
+// Si agregamos los endpoints especificos despues de los dinamicos, la url no funcionará
+
+
+// desde la request podemos extraer parametros y usando destructuracion ECMAScript
+// tomamos el id para usarlo en nuestra response
+app.get('/matches/:id', (req, res) => {
   const { id } = req.params;
   res.json({
     id,
@@ -39,6 +74,19 @@ app.get('/partidos/:id', (req, res) => {
   });
 });
 
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('No hay parametros')
+  }
+});
+
+// Tambien podemos crear urls mas complejas con los id que queramos
 app.get('/matches/:matchId/bets/:betId', (req, res) => {
   const { matchId, betId } = req.params;
   res.json({
